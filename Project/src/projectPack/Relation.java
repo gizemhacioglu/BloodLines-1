8c74518c2391dd1f1bd993875dbf799ff966465c
Alperen DERİCİ <alperen.derici@std.izmirekonomi.edu.tr>
2022-01-08 23:48:08 +0300

    Relation class's variables are created
    With constructor

--- Project/src/projectPack/Relation.java
+++ Project/src/projectPack/Relation.java
@@ -1,4 +1,21 @@
 package projectPack;
 
+import java.util.ArrayList;
+
 public class Relation {
+    Person spouse1;
+    Person spouse2;
+    static int relCounter = 1;
+    int id;
+
+    ArrayList<Relation> relations = new ArrayList<Relation>();
+    ArrayList<Person> children = new ArrayList<Person>();
+
+
+    public Relation(Person spouse1, Person spouse2) {
+        this.spouse1 = spouse1;
+        this.spouse2 = spouse2;
+        id = relCounter;
+        relCounter++;
+    }
 }
