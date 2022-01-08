8d5253c62dfc34316dbf3ce47e3848233bd47566
Alperen DERİCİ <alperen.derici@std.izmirekonomi.edu.tr>
2022-01-08 23:39:30 +0300

    Person class's variables are created
    With constructor and getter setter methods. Also psvm included in Main class.

--- Project/src/projectPack/Main.java
+++ Project/src/projectPack/Main.java
@@ -1,4 +1,7 @@
 package projectPack;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- Project/src/projectPack/Person.java
+++ Project/src/projectPack/Person.java
@@ -1,4 +1,68 @@
 package projectPack;
 
+import java.util.ArrayList;
+
 public class Person {
+    static int personCounter = 0;
+    int id;
+    Relation parents;
+    String name;
+    String surname;
+    String birthday;
+    Boolean gender;
+
+    ArrayList<Person> persons = new ArrayList<Person>();
+
+    public Person(projectPack.Relation parents, String name, String surname, String birthday, Boolean gender){
+        this.parents = parents;
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.gender = gender;
+        id = personCounter;
+        personCounter++;
+    }
+
+
+    public Relation getParents() {
+        return parents;
+    }
+
+    public void setParents(Relation parents) {
+        this.parents = parents;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+
 }
